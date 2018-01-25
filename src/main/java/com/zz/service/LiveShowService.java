61c3b4407f92b61b3ac55830b9ad70296f806fd5
Lehtean <1434745143@qq.com>
2018-01-25 21:02:44 +0800

    增加我的关注

--- src/main/java/com/zz/service/LiveShowService.java
+++ src/main/java/com/zz/service/LiveShowService.java
@@ -13,12 +13,21 @@ public interface LiveShowService {
 
     /**
      * 获取所有直播列表
+     * @param parameter 包括搜索 分页 类型等
      * @return
      */
     List<LiveShow> getLiveList(Parameter parameter);
 
     /**
+     * 获取所有关注直播列表
+     * @param id 用户id
+     * @return
+     */
+    List<LiveShow> getFollowList(int id);
+
+    /**
      * 获取直播详情
+     * @param id room_id
      * @return
      */
     LiveShow getLiveDetail(int id);
